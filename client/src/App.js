7bd926a8eb874f29294c8e1b3d499944e3ef698e
Cameron Nye <csnye86@gmail.com>
2020-11-14 03:20:08 -0700

    Created global context

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, {useState} from "react"
 import { Route, Switch } from "react-router-dom"
 import LandingPage from "./components/LandingPage"
 import SignUp from "./components/SignUp/index"
@@ -9,25 +9,33 @@ import ClientManager from "./components/ClientManager/index"
 import WorkoutManager from "./components/WorkoutManager/index"
 import ExerciseManager from "./components/ExerciseManager/index"
 import Dashboard from "./components/Dashboard/index"
+import GlobalContext from "./context/GlobalContext"
 import "./App.scss"
 
 const App = () => {
+  const [user, setUser] = useState({})
+
 	return (
-		<div className="app">
-			{window.location.pathname !== "/" &&
-			window.location.pathname !== "/sign-up" &&
-			window.location.pathname !== "/login" && <Nav />}
-			<Switch>
-				<Route exact path="/" component={LandingPage} />
-				<Route exact path="/login" component={Login} />
-				<Route exact path="/sign-up" component={SignUp} />
-				<Route exact path="/schedule-workout" component={Session} />
-				<Route exact path="/clients" component={ClientManager} />
-				<Route exact path="/workout-manager" component={WorkoutManager} />
-				<Route exact path="/exercise-manager" component={ExerciseManager} />
-				<Route exact path="/dashboard" component={Dashboard} />
-			</Switch>
-		</div>
+		<GlobalContext.Provider
+      value={{
+        user,
+        setUser
+      }}
+    >
+			<div className="app">
+				{Object.keys(user).length > 0 && <Nav />}
+				<Switch>
+					<Route exact path="/" component={LandingPage} />
+					<Route exact path="/login" component={Login} />
+					<Route exact path="/sign-up" component={SignUp} />
+					<Route exact path="/schedule-workout" component={Session} />
+					<Route exact path="/clients" component={ClientManager} />
+					<Route exact path="/workout-manager" component={WorkoutManager} />
+					<Route exact path="/exercise-manager" component={ExerciseManager} />
+					<Route exact path="/dashboard" component={Dashboard} />
+				</Switch>
+			</div>
+		</GlobalContext.Provider>
 	)
 }
 
