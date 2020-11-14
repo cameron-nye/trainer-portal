3dbd43567cc9191a0f4c8fd7b85518e0d37f84be
Cameron Nye <csnye86@gmail.com>
2020-11-14 03:22:05 -0700

    WIP: sign up functionality

--- client/src/components/SignUp/index.js
+++ client/src/components/SignUp/index.js
@@ -1,21 +1,35 @@
-import React, { useState } from "react"
+import React, { useState, useContext } from "react"
 import {useHistory} from 'react-router-dom'
 import axios from 'axios'
 
 import "./index.scss"
+import GlobalContext from "../../context/GlobalContext"
 
 const SignUp = () => {
 	const [ firstName, setFirstName ] = useState("")
 	const [ lastName, setLastName ] = useState("")
-	const [ userName, setUserName ] = useState("")
-  const [ trainerName, setTrainerName ] = useState("")
+	const [ username, setUsername ] = useState("")
+  const [ trainerUsername, setTrainerUsername ] = useState("")
 
   const history = useHistory()
+
+  const {user, setUser} = useContext(GlobalContext)
   
   // NEED TO FILL IN WITH ENDPOINT DATA
   const handleSubmit = () => {
-    // axios.post('')
-    history.push('/dashboard')
+    axios
+      .post('http://localhost:8080/users', {firstName, lastName, username, trainerUsername})
+      .then(res => {
+        setUser(...user, {
+          userId: res.data.userId,
+          firstName,
+          lastName,
+          username,
+          trainerUsername
+        })
+        history.push('/dashboard')
+      })
+      .catch(err => console.log(err))
   }
 
 	return (
@@ -24,8 +38,8 @@ const SignUp = () => {
 				<h1>Sign Up</h1>
 				<input type="text" placeholder="First Name" onChange={e => setFirstName(e.target.value)} />
 				<input type="text" placeholder="Last Name" onChange={e => setLastName(e.target.value)} />
-				<input type="text" placeholder="Username" onChange={e => setUserName(e.target.value)} />
-				<input type="text" placeholder="Trainer Name" onChange={e => setTrainerName(e.target.value)} />
+				<input type="text" placeholder="Username" onChange={e => setUsername(e.target.value)} />
+				<input type="text" placeholder="Trainer Username" onChange={e => setTrainerUsername(e.target.value)} />
 				<button onClick={handleSubmit}>Sign Up</button>
 			</div>
 		</div>
