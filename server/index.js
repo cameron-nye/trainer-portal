144de8afad1aa6260774e597fff4e9047e28815b
Kyler Prisbrey <kprisbrey@wilsonelectronics.com>
2020-11-14 00:08:27 -0700

    Added Exercise

--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ import cors from 'cors'
 
 import userRoutes from './routes/users.js';
 import sessionRoutes from './routes/sessions.js';
+import exerciseRoutes from './routes/exercises.js';
 
 const app = express();
 
@@ -13,6 +14,7 @@ app.use(cors());
 
 app.use('/users', userRoutes);
 app.use('/sessions', sessionRoutes);
+app.use('/exercises', exerciseRoutes);
 
 const PORT = process.env.PORT || 8081;
 
