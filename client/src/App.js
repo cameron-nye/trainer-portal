9009783d8e6110e91cb8ba22392e6fb856732254
Cameron Nye <csnye86@gmail.com>
2020-11-14 00:36:17 -0700

    Nav done

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,32 @@
-import React from 'react'
-import {Route, Switch} from 'react-router-dom'
-import LandingPage from './components/LandingPage'
-import SignUp from './components/SignUp/index'
-import Login from './components/Login/index'
-import './App.scss'
+import React from "react"
+import { Route, Switch } from "react-router-dom"
+import LandingPage from "./components/LandingPage"
+import SignUp from "./components/SignUp/index"
+import Login from "./components/Login/index"
+import Nav from "./components/Nav/index"
+import Session from './components/Session/index'
+import ClientManager from './components/ClientManager/index'
+import WorkoutManager from './components/WorkoutManager/index'
+import ExerciseManager from './components/ExerciseManager/index'
+import "./App.scss"
 
 const App = () => {
-
-  return (
-    <div className='app'>
-      <Switch>
-        <Route exact path='/' component={LandingPage}/>
-        <Route exact path='/login' component={Login}/>
-        <Route exact path='/sign-up' component={SignUp}/>
-      </Switch>
-    </div>
-  )
+	return (
+		<div className="app">
+			{window.location.pathname !== "/" &&
+			window.location.pathname !== "/sign-up" &&
+			window.location.pathname !== "/login" && <Nav />}
+			<Switch>
+				<Route exact path="/" component={LandingPage} />
+				<Route exact path="/login" component={Login} />
+				<Route exact path="/sign-up" component={SignUp} />
+				<Route exact path="/schedule-workout" component={Session} />
+				<Route exact path="/clients" component={ClientManager} />
+				<Route exact path="/workout-manager" component={WorkoutManager} />
+				<Route exact path="/exercise-manager" component={ExerciseManager} />
+			</Switch>
+		</div>
+	)
 }
 
 export default App
--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -1,11 +1,92 @@
-import React from 'react'
+import React, { useState, useRef, useEffect } from "react"
+import { HiMenuAlt4, HiOutlineX } from "react-icons/hi"
+
+import "./index.scss"
 
 const Nav = () => {
-  return (
-    <div>
-      Nav
-    </div>
-  )
+	const [ isOpen, setIsOpen ] = useState(true)
+	const drawer = useRef(null)
+
+	useEffect(
+		() => {
+			if (!isOpen) return
+			const handleOffClick = e => {
+				const clickedInDrawer = e.target === drawer.current || e.target === drawer.current.contains(e.target)
+				if (clickedInDrawer) return
+				setIsOpen(false)
+			}
+			window.addEventListener("click", handleOffClick)
+			return () => window.removeEventListener("click", handleOffClick)
+		},
+		[ isOpen ]
+	)
+
+	const navLinks = [
+		{
+			title: "Schedule Workout",
+			href: "/schedule-workout",
+			navSection: "top"
+		},
+		{
+			title: "Clients",
+			href: "/clients",
+			navSection: "top"
+		},
+		{
+			title: "Workouts",
+			href: "/workout-manager",
+			navSection: "top"
+		},
+		{
+			title: "Exercises",
+			href: "/exercise-manager",
+			navSection: "top"
+		},
+		{
+			title: "Profile",
+			href: "/profile",
+			navSection: "bottom"
+		},
+		{
+			title: "Sign Out",
+			href: "/",
+			navSection: "bottom"
+		}
+	]
+
+	return (
+		<div className="nav">
+			<HiMenuAlt4 onClick={() => setIsOpen(true)} color={"#F2D479"} size={24} className="icon" />
+			<h1>
+				<a href="/">
+					Athena Fitness
+				</a>
+			</h1>
+			<div className={`drawer ${isOpen && "open"}`} ref={drawer}>
+				<ul className="top">
+					<li onClick={() => setIsOpen(false)}>
+						<HiOutlineX className="icon" color={"#313640"} size={24} onClick={() => setIsOpen(false)} />
+					</li>
+					{navLinks.filter(nl => nl.navSection === "top").map((nl, i) => {
+						return (
+							<li key={i}>
+								<a href={nl.href}>{nl.title}</a>
+							</li>
+						)
+					})}
+				</ul>
+				<ul className="bottom">
+					{navLinks.filter(nl => nl.navSection === "bottom").map((nl, i) => {
+						return (
+							<li key={i}>
+								<a href={nl.href}>{nl.title}</a>
+							</li>
+						)
+					})}
+				</ul>
+			</div>
+		</div>
+	)
 }
 
 export default Nav
