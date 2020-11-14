1f67f7bcb21d47dffb117a43da79d26f834e9f02
Cameron Nye <csnye86@gmail.com>
2020-11-13 22:38:46 -0700

    WIP: Need to connect POST

--- client/src/components/SignUp/index.js
+++ client/src/components/SignUp/index.js
@@ -1,13 +1,35 @@
-import React from 'react'
+import React, { useState } from "react"
+import {useHistory} from 'react-router-dom'
+import axios from 'axios'
 
-import './index.scss'
+import "./index.scss"
 
 const SignUp = () => {
-  return (
-    <div>
-      Sign Up
-    </div>
-  )
+	const [ firstName, setFirstName ] = useState("")
+	const [ lastName, setLastName ] = useState("")
+	const [ userName, setUserName ] = useState("")
+  const [ trainerName, setTrainerName ] = useState("")
+
+  const history = useHistory()
+  
+  // NEED TO FILL IN WITH ENDPOINT DATA
+  const handleSubmit = () => {
+    // axios.post('')
+    history.push('/dashboard')
+  }
+
+	return (
+		<div className="sign-up">
+			<div>
+				<h1>Sign Up</h1>
+				<input type="text" placeholder="First Name" onChange={e => setFirstName(e.target.value)} />
+				<input type="text" placeholder="Last Name" onChange={e => setLastName(e.target.value)} />
+				<input type="text" placeholder="Username" onChange={e => setUserName(e.target.value)} />
+				<input type="text" placeholder="Trainer Name" onChange={e => setTrainerName(e.target.value)} />
+				<button onClick={handleSubmit}>Sign Up</button>
+			</div>
+		</div>
+	)
 }
 
 export default SignUp
